6109089aab6e4fd1146eb2a8f926d6b62ec973fb
Shu Maung <shumaung004@gmail.com>
2025-08-23 18:02:06 +0700

    second commit

---
+++ linked_list/linked_list_02.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
+    def append(self,value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+    
+    def __str__(self):
+        result = []
+        temp_node = self.head
+        while temp_node:
+            result.append(str(temp_node.value))
+            temp_node = temp_node.next
+        return "->".join(result)
+    
+new_linked_list = LinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+print(new_linked_list)
