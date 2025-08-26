d34f9e5590b155c362cf06d7ee48deb3f373662f
Shu Maung <shumaung004@gmail.com>
2025-08-27 02:26:47 +0700

    added methods

--- linked_list/linked_list/linked_list_01.py
+++ linked_list/linked_list/linked_list_01.py
@@ -49,6 +49,86 @@ class LinkedList:
         self.length += 1
         return True
     
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+    
+    def search(self,target):
+        current = self.head
+        while current:
+            if current.value == target:
+                return True
+            current = current.next
+        return False
+    
+    def get(self, index):
+        if index == -1:
+            return self.tail
+        if index < -1 or index >= self.length:
+            return None
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        popped_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            # return popped_node
+        else:
+            self.head = self.head.next
+            popped_node.next = None
+        self.length -= 1
+        return popped_node
+        
+    def pop(self):
+        if self.length == 0:
+            return None
+        popped_node = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            while temp.next is not self.tail:
+                temp = temp.next
+            self.tail = temp
+            self.tail.next = None
+        self.length -= 1
+        return popped_node
+    
+    def remove(self, index):
+        if index >= self.length or index < -1:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1 or index == -1:
+            return self.pop()
+        prev_node = self.get(index - 1)
+        popped_node = prev_node.next
+        prev_node.next = popped_node.next
+        popped_node.next = None
+        self.length -= 1
+        return popped_node
+    
+    def remove_all(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    
     def __str__(self):
         temp_node = self.head
         result = ""
@@ -63,4 +143,6 @@ new_linked_list = LinkedList()
 new_linked_list.append(10)
 new_linked_list.append(20)
 new_linked_list.append(30)
+# print(new_linked_list.get(1))
+new_linked_list.remove(1)
 print(new_linked_list)
--- linked_list/linked_list/linked_list_02.py
+++ linked_list/linked_list/linked_list_02.py
@@ -21,13 +21,13 @@ class LinkedList:
         self.length += 1
     
     def prepend(self,value):
-        new_mode = Node(value)
+        new_node = Node(value)
         if not self.head:
-            self.head = new_mode
-            self.tail = new_mode
+            self.head = new_node
+            self.tail = new_node
         else:
-            new_mode.next = self.head
-            self.head = new_mode
+            new_node.next = self.head
+            self.head = new_node
         self.length += 1
     
     def insert(self, index, value):
@@ -45,10 +45,89 @@ class LinkedList:
             for _ in range(index - 1):
                 temp_node = temp_node.next
             new_node.next = temp_node.next
-            # print(temp_node.value)
             temp_node.next = new_node
         return True
 
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
+    def search(self,target):
+        current = self.head
+        index = 0
+        while current:
+            if current.value == target:
+                return True
+            current = current.next
+            index += 1
+        return False
+    
+    def get(self,index):
+        if index == -1:
+            return self.tail
+        if index < -1 or index >= self.length:
+            return None
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current
+    
+    def set_values(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        popped_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        popped_node = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            temp = self.head
+            while temp.next is not self.tail:
+                temp = temp.next
+            temp.next = None
+            self.tail = None
+        self.length -= 1
+        return popped_node
+    
+    def remove(self,index ):
+        if index >= self.length or index < -1:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1 or index == -1:
+            return self.pop()
+        prev_node = self.get(index - 1)
+        prev_node.next = popped_node.next
+        popped_node = prev_node.next
+        popped_node.next = None
+        self.length -= 1
+        return popped_node
+
+    def remove_all(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
     
     def __str__(self):
         result = []
@@ -63,7 +142,10 @@ new_linked_list.append(10)
 new_linked_list.append(20)
 new_linked_list.append(40)
 new_linked_list.append(50)
-new_linked_list.insert(2,30)
-print(new_linked_list)
+# new_linked_list.insert(2,30)
+# print(new_linked_list)
+# new_linked_list.search(50)
+# print(" ==== " ,new_linked_list.search(50))
+new_linked_list.traverse()
 
 # 10 -> 20 -> 40 -> 50
